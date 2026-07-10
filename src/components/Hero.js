dd798de3d06b023f543189b8997e0843c1da942d
Aryan Sengar <151959764+aryansengar007@users.noreply.github.com>
2026-07-11 00:12:09 +0530

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => (
 
     <h1>Hi, I am Aryan Sengar</h1>
     <div className="cta-buttons">
-      <a href="https://drive.google.com/file/d/1X2BYcPlmSUZa4RIqfeWUIm2-UKfPNCUp/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn">View Resume</a> |
+      <a href="https://drive.google.com/file/d/1WLpfuvaEHHPWinTTrXMnsJ_jfpsD2gtg/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn">View Resume</a> |
       <a href="/projects" className="btn"> View Projects</a> |
       <a href="/contact" className="btn"> Contact Me</a> |
       <a href="https://github.com/aryansengar007" target="_blank" rel="noopener noreferrer" className="btn"> GitHub</a> |
@@ -22,6 +22,5 @@ const Hero = () => (
     </div>
   </motion.section>
 );
-
 
 export default Hero;
